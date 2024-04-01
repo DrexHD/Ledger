63400054eed31e07a39f044f9388ebca7c1d0abd
Drex <nicknamedrex@gmail.com>
2024-04-01 16:50:48 +0200

    Update to 24w14potato

--- src/main/java/com/github/quiltservertools/ledger/mixin/entities/LivingEntityMixin.java
+++ src/main/java/com/github/quiltservertools/ledger/mixin/entities/LivingEntityMixin.java
@@ -1,12 +1,18 @@
 package com.github.quiltservertools.ledger.mixin.entities;
 
 import com.github.quiltservertools.ledger.callbacks.EntityKillCallback;
+import com.github.quiltservertools.ledger.callbacks.ItemDropCallback;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
@@ -20,5 +26,12 @@ public abstract class LivingEntityMixin {
         EntityKillCallback.EVENT.invoker().kill(
                 entity.getWorld(), entity.getBlockPos(), entity, source
         );
+    }
+
+    @Inject(method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ItemEntity;setPickupDelay(I)V"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private void ledgerLogEntityItemDrop(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir, double d, ItemEntity itemEntity) {
+        if ((Object) this instanceof PlayerEntity player) {
+            ItemDropCallback.EVENT.invoker().drop(itemEntity, player);
+        }
     }
 }
